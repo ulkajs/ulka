aa85142e9d3935129c1376c2006883ae12a778e9
Roshan Acharya <acharyaroshan2357@gmail.com>
2021-05-17 13:52:30 +0545

    feat: add ulka template support

---
+++ packages/ulka/src/Templates/UlkaTemplate.ts
@@ -0,0 +1,25 @@
+import * as ulka from '@ulka/template-engine'
+
+import { Template } from './Template'
+
+export class UlkaTemplate extends Template {
+  static ulka = ulka
+
+  async compile() {
+    return async (ctx: object = {}) => {
+      return UlkaTemplate.ulka.render(
+        this.content as string,
+        { ...this.context, ...ctx },
+        { base: this.collection.ulka.configs.include }
+      )
+    }
+  }
+
+  get hasMatter() {
+    return true
+  }
+
+  get buildExt() {
+    return '.html'
+  }
+}
