9b674662982fe47288c4447d16e785facf7b7a8e
Roshan Acharya <acharyaroshan2357@gmail.com>
2021-05-22 20:12:08 +0545

    fix(plugin-sass): removed cache cause my implementation of cache sucked

--- packages/plugin-sass/src/index.ts
+++ packages/plugin-sass/src/index.ts
@@ -2,18 +2,12 @@ import fs from 'fs'
 import sass from 'sass'
 import { Template, PluginFunction } from 'ulka'
 
-// cache filepath, sass and css
-const cache: { [key: string]: { sass: string; css: Buffer } } = {}
-
 class SassTemplate extends Template {
   static sass = sass
   static opts: sass.Options = {}
 
   async compile() {
     return async () => {
-      if (cache[this.fileinfo.filepath]?.sass === this.content)
-        return cache[this.fileinfo.filepath].css
-
       const result = SassTemplate.sass.renderSync({
         data: this.content as string,
         file: this.fileinfo.filepath,
@@ -24,11 +18,6 @@ class SassTemplate extends Template {
 
       if (result.map) {
         fs.writeFileSync(this.buildPath + '.map', result.map)
-      }
-
-      cache[this.fileinfo.filepath] = {
-        sass: this.content as string,
-        css: result.css,
       }
 
       return result.css
