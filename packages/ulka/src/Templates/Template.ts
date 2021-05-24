15383bd9c0c9afdc7f39278d3db50e848b6f44fe
Roshan Acharya <acharyaroshan2357@gmail.com>
2021-05-24 13:53:05 +0545

    docs: add comment to layout method in Template class as I was getting confused about what was happening there

--- packages/ulka/src/Templates/Template.ts
+++ packages/ulka/src/Templates/Template.ts
@@ -3,7 +3,7 @@ import util from 'util'
 import path from 'path'
 import c from 'ansi-colors'
 import matter from 'gray-matter'
-import * as utemp from '@ulkajs/template-engine'
+import * as ulkaTemplate from '@ulkajs/template-engine'
 
 import { UlkaError } from '../UlkaError'
 
@@ -17,6 +17,7 @@ export class Template {
   public content: string | Buffer = ''
   public link: string = ''
   public buildPath: string = ''
+  public dontBuild = false
 
   public context: { [key: string]: any } = {}
 
@@ -38,44 +39,55 @@ export class Template {
   ): Promise<string | Buffer> {
     const matter = this.context.matter
 
+    // if matter._layout `false` then return content
+    if (matter._layout === false) return content
+
+    // if there is no layout collection or no layout directory then return content
     if (!this.ulka.layout || !this.ulka.configs.layout) return content
 
+    // if matter._layout is not defined && no layout passed from layout content then return content
     if (!matter._layout && !this.configLayout) return content
 
-    const matterLayout = matter._layout
+    // if templateSpecialFrontMatter is true render matter._layout with @ulkajs/template-engine
+    // give first priority to matter._layout and then from config
+    const base = this.ulka.configs.include
+    const _layout = matter._layout
       ? this.ulka.configs.templateSpecialFrontMatter
-        ? utemp.render(matter._layout, this.context, {
-            base: this.ulka.configs.include,
-          })
+        ? ulkaTemplate.render(matter._layout, this.context, { base })
         : matter._layout
-      : null
+      : typeof this.configLayout === 'function'
+      ? this.configLayout(this)
+      : this.configLayout
 
-    const _layout =
-      matterLayout ||
-      (typeof this.configLayout === 'function'
-        ? this.configLayout(this)
-        : this.configLayout)
-
+    // if _layout is not string return content
+    // for eg. configLayout function ma return anything execept function
     if (typeof _layout !== 'string') return content
 
+    // absolute layout path to _layout
     const lpath = path.join(this.ulka.configs.layout, _layout)
 
     try {
+      // find the layout in layout collection
+      // if not found return content
       const tpl = this.ulka.layout.contents.find(
         (v: Template) => v.fileinfo.filepath === lpath
       )
       if (!tpl) return content
 
+      // if layout is found in layoutcache then renderFunc will be that cache
+      // else compile the layout and cache the renderFunc
       let renderFunc = this.ulka.layoutFuncCache[lpath]
-
       if (!renderFunc) {
         renderFunc = await tpl.compile()
         this.ulka.layoutFuncCache[lpath] = renderFunc
       }
 
+      // current template context is inside _ cause layout will have its own context
+      // _ in layout will be reference to current template context
       const _ = { ...this.context, content, ...ctx }
       const contentWithLayout = await renderFunc({ _ })
 
+      // recurse the current function incase layout has another layout
       return tpl.layout(contentWithLayout, { _ })
     } catch (e) {
       const cwd = this.ulka.cwd
@@ -93,8 +105,8 @@ export class Template {
     try {
       await mkdir(path.parse(this.buildPath).dir, { recursive: true })
 
-      const fn = await this.compile()
-      const content = await fn(ctx)
+      const renderFunction = await this.compile()
+      const content = await renderFunction(ctx)
       this.content = await this.layout(content)
     } catch (e) {
       const cwd = this.ulka.cwd
@@ -113,9 +125,8 @@ export class Template {
     try {
       await writeFile(this.buildPath, content)
     } catch (e) {
-      const cwd = this.ulka.cwd
-      const rbpath = path.relative(cwd, this.buildPath)
-      const rfpath = path.relative(cwd, this.fileinfo.filepath)
+      const rbpath = path.relative(this.ulka.cwd, this.buildPath)
+      const rfpath = path.relative(this.ulka.cwd, this.fileinfo.filepath)
 
       throw new UlkaError(
         e.message,
@@ -145,9 +156,11 @@ export class Template {
       ...ctx,
     }
 
+    // if matter has _link prop and templateSpecialFrontMatter is turned on
+    // render matter._link with @ulkajs/template-engine
     if (matter._link) {
       link = this.ulka.configs.templateSpecialFrontMatter
-        ? utemp.render(matter._link, this.context, {
+        ? ulkaTemplate.render(matter._link, this.context, {
             base: this.ulka.configs.include,
           })
         : matter._link
@@ -203,6 +216,11 @@ export class Template {
 
     let name = this.fileinfo.parsedpath.name
 
+    // if ext = html and name != index.html then create folder with name and change name to index
+    // for eg:
+    // if file is about.html create a folder named about
+    // and a file inside about as index.html
+    // about.html => about/index.html
     if (ext === '.html') {
       if (name !== 'index') {
         rel = path.join(rel, name)
