1ea32267f2d60d516d59cc1b571d4d130066bae9
Roshan Acharya <acharyaroshan2357@gmail.com>
2023-12-17 18:44:42 +0545

    feat(template-engine): support for components

--- packages/template-engine/src/index.ts
+++ packages/template-engine/src/index.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export interface Options {
   base: string
+  component?: boolean
 }
 
 export { render }
@@ -11,16 +12,22 @@ export { render }
 function render(
   str: string,
   data: { [key: string]: any } = {},
-  options: Options = { base: process.cwd() }
+  options: Options = { base: process.cwd(), component: false }
 ) {
   const ctx = vm.createContext({
     ...data,
     require: (path: string) => requireFunction(path, options),
     include: (path: string) => includeFunction(path, options, ctx),
+    defineComponent: (path: string, name: string) =>
+      defineComponentFunction(path, name, options, ctx),
     console: { log: console.log },
   })
 
-  return renderInContext(str, ctx)
+  let res = renderInContext(str, ctx)
+
+  if (options.component) res = withComponent(res, ctx)
+
+  return res
 }
 
 function renderInContext(str: string, ctx: vm.Context) {
@@ -62,11 +69,11 @@ function includeFunction(
   const absIncPath = path.join(options.base, includePath)
   const tpl = fs.readFileSync(absIncPath, 'utf-8')
 
-  patchRequireAndInclude(ctx, { base: path.dirname(absIncPath) })
+  patchRequireAndInclude(ctx, { ...options, base: path.dirname(absIncPath) })
 
   const data = renderInContext(tpl, ctx)
 
-  patchRequireAndInclude(ctx, { base: options.base })
+  patchRequireAndInclude(ctx, { ...options, base: options.base })
 
   return data
 }
@@ -74,4 +81,53 @@ function includeFunction(
 function patchRequireAndInclude(ctx: vm.Context, options: Options) {
   ctx.require = (path: string) => requireFunction(path, options)
   ctx.include = (path: string) => includeFunction(path, options, ctx)
+  ctx.defineComponent = (path: string, name: string) =>
+    defineComponentFunction(path, name, options, ctx)
+}
+
+function defineComponentFunction(
+  cmpPath: string,
+  name: string,
+  options: Options,
+  ctx: vm.Context
+) {
+  if (!name) name = path.parse(cmpPath).name
+
+  const absIncPath = path.join(options.base, cmpPath)
+  const tpl = fs.readFileSync(absIncPath, 'utf-8')
+
+  ctx[name] = (props: Record<string, any>) => {
+    patchRequireAndInclude(ctx, { ...options, base: path.dirname(absIncPath) })
+
+    const newCtx = vm.createContext(ctx)
+    newCtx.props = props
+    const data = renderInContext(tpl, newCtx)
+
+    patchRequireAndInclude(ctx, { ...options, base: options.base })
+
+    return data
+  }
+}
+
+function withComponent(code: string, ctx: vm.Context) {
+  const self = /<u-([a-z]+) ([a-zA-z ]*) *\/>/
+  const sep = /<u-([a-z]+) *([a-zA-z ]*) *>(.*)<\/u-\1>/s
+
+  code = code.replace(self, (_, name, pstr) => {
+    const props = pstr.trim().split(' ').join(', ')
+    return vm.runInContext(`${name}({ ${props} })`, ctx)
+  })
+
+  code = code.replace(sep, (_, name, pstr, children) => {
+    let props = pstr.trim().split(' ').join(', ')
+    children = children.trim()
+
+    if (props !== '') props += ', '
+
+    props += 'children: `' + children + '`'
+
+    return vm.runInContext(`${name}({ ${props} })`, ctx)
+  })
+
+  return code
 }
